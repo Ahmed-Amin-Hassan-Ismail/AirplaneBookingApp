353d64837419edecb1e43f9e9800bf6295ab0d1f
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-08-20 16:50:17 +0300

    initial commit create xCode project

---
+++ AirplaneBooking/AirplaneBooking/AirplaneBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirplaneBookingApp.swift
+//  AirplaneBooking
+//
+//  Created by Ahmed Amin on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirplaneBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
